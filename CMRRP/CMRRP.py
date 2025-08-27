22c1776ed5edd6fc45eb3ada4e64aa3c3e18f7c6
Huang <zhuang480@gatech.edu>
2025-08-27 16:58:12 -0400

    BFS Planning Waypoint 2

--- CMRRP/CMRRP.py
+++ CMRRP/CMRRP.py
@@ -15,9 +15,15 @@ class CGFManager:
     m: int = 7                              # Number of modules in a configuration
 
     # The initialization methods should only be called for the root node, once
-    def __init__(self, Gamma_final: list, correspondence: list = None, 
-                 gf_ang_list: list = None, gt_ang_list: list = None):
+    def __init__(self, Gamma_final: list, 
+                 survival_idx: list = None, correspondence: list = None, 
+                 gf_idx_list: list = None, gt_idx_list: list = None):
         self.Gamma_final = Gamma_final
+        
+        if survival_idx is None: 
+            self.survival_idx = [i for i in range(len(Gamma_final))]
+        else:
+            self.survival_idx = survival_idx
 
         # correspondence[2 * i + 0] = 2 * j + 1 => i's head is j's tail in target gmrc
         if correspondence is None:
@@ -25,29 +31,76 @@ class CGFManager:
         else:
             self.correspondence = correspondence
 
-        # Lists storing the appropriate angle *if* gf is grasping gt
-        #   NOTE: Which means that the number of grips here is redundant
-        #         For example, 3H==30°==>6T and 6T==-30°==>3H both exist
-        if gf_ang_list is None:             # gf_ang_list[2 * i + ht] = (ang, gt)
-            gf_ang_list = [None] * (2 * CGFManager.m)
-            for gamma in self.Gamma_final:
-                pass
-        else:
-            self.gf_ang_list = gf_ang_list
-        if gt_ang_list is None:             # gt_ang_list[2 * i + ht] = (ang, gf)
-            gt_ang_list = [None] * (2 * CGFManager.m)
-        else:
-            self.gt_ang_list = gt_ang_list
+        if gf_idx_list is None or gt_idx_list is None: 
+            gf_idx_list = [[]] * (2 * CGFManager.m)
+            gt_idx_list = [[]] * (2 * CGFManager.m)
+            for i in self.survival_idx:
+                gamma_final = self.Gamma_final[i]
+                gf = gamma_final[1]
+                gt = gamma_final[2]
+                gf_idx_list[gf].append(i)
+                gt_idx_list[gt].append(i)
+        self.gf_idx_list = gf_idx_list      # If chosen as gf, what angle can it choose
+        self.gt_idx_list = gt_idx_list      # If chosen as gt, what angle can it choose
 
-    # TODO: If gt's correspondence is not established and gt is on w-grip layer-2
-    #       Then return None, indicating that should not allow choosing from Gamma_final
-    def get_Gamma_final_angle(self, gf, gt):    # Get the appropriate Gamma_final angle
-        pass
+    # Collect an angle given Gamma_final index, and build correspondence based on it
+    def get_angle(self, new_gf, new_gt, index):
+        gamma_final = self.Gamma_final[index]
+        angle = gamma_final[0]
+        gf = gamma_final[1]
+        gt = gamma_final[2]
+        bias_index = gamma_final[3]
+        is_w_grip = gamma_final[4]
+
+        self.correspondence[new_gf] = gf
+        self.correspondence[
+            2 * (new_gf // 2) + 1 - new_gf % 2] = 2 * (gf // 2) + 1 - gf % 2
+        self.correspondence[new_gt] = gt
+        self.correspondence[
+            2 * (new_gt // 2) + 1 - new_gt % 2] = 2 * (gt // 2) + 1 - gt % 2
+        
+        if not is_w_grip:
+            self.survival_idx[index - bias_index : index - bias_index + 2] = []
+            self.gf_idx_list[gf] = []
+            self.gf_idx_list[gt] = []
+            self.gt_idx_list[gt] = []
+            self.gt_idx_list[gf] = []
+        else:
+            keep_idx = (index - bias_index) + (bias_index + 2) % 6
+            self.survival_idx[index - bias_index : index - bias_index + 6] = [keep_idx]
+            self.gf_idx_list[gf] = []           # gf can no longer grasp any gripper
+            self.gf_idx_list[gt] = []           # gt can no longer grasp any gripper
+            self.gt_idx_list[gt] = []           # gt can no longer be grasper
+            self.gt_idx_list[gf] = [keep_idx]   # gf can only be grasped with keep_idx
+            
+        return angle
+
+    # Get the appropriate Gamma_final angle given grippers in new gmrc (not in target)
+    #   Each returned index collects a angle from cgf_manager to build correspondence
+    def get_angle_idxes(self, new_gf, new_gt, is_w_grip):
+        gf = self.correspondence[new_gf]
+        gt = self.correspondence[new_gt]
+        if gt < 0 and is_w_grip:            # If grasp a uncorresponded outer gripper
+            return []                       # Only can build correspondence bottom->top
+        elif gf < 0 and gt < 0:             # If both grippers are uncorresponded
+            return self.survival_idx
+        elif gf >= 0 and gt < 0:            # If only gripper_from is corresponded
+            return self.gf_idx_list[gf]
+        elif gf < 0 and gt >= 0:            # If only gripper_to is corresponded
+            return self.gt_idx_list[gt]
+        else:                               # If both grippers are corresponded
+            idx_list = []
+            for idx in self.gf_idx_list[gf]:
+                if self.Gamma_final[idx][2] == gt:
+                    idx_list.append(idx)
+            return idx_list
 
     def copy(self):                         # Corresponding will not change ang_lists
-        return CGFManager(copy.deepcopy(self.Gamma_final),
-                          copy.deepcopy(self.correspondence),
-                          self.gf_ang_list, self.gt_ang_list)
+        return CGFManager(Gamma_final=self.Gamma_final,
+                          survival_idx=copy.deepcopy(self.survival_idx),
+                          correspondence=copy.deepcopy(self.correspondence),
+                          gf_idx_list=copy.deepcopy(self.gf_idx_list),
+                          gt_idx_list=copy.deepcopy(self.gt_idx_list))
 
 # A search tree node contains: 1. a unique gmrc shape; 2. a partial correspondence
 class TreeNode:
@@ -78,7 +131,6 @@ class TreeNode:
                     continue
                 self.children.extend(
                     self._get_all_memebers_in_grasping_group(new_gmrc, action))
-
         self.expanded = True
 
     # Get all reasonable children from the same grasping action based on eldest sibling
@@ -93,7 +145,36 @@ class TreeNode:
         gf = action[0]
         gt = action[1]
         grip = new_gmrc.module2gripper[gf % 2][gf // 2] // 3
-        all_Gamma_final_angs = self.cgf_manager.get_Gamma_final_angle(gf, gt)
+        mid_ang = new_gmrc.get_grip_gamma(grip)
+
+        min_gmrc = new_gmrc.copy()
+        min_gmrc.modify_grsp_ang(grip, -GMRC.grsp_ang_cap)
+        min_ang = min_gmrc.get_grip_gamma(grip)
+        if min_ang < mid_ang:
+            min_node = TreeNode(min_gmrc, self.cgf_manager.copy(),
+                                self, self.g_depth + 1)
+            members.append(min_node)
+
+        max_gmrc = new_gmrc.copy()
+        max_gmrc.modify_grsp_ang(grip, GMRC.grsp_ang_cap)
+        max_ang = max_gmrc.get_grip_gamma(grip)
+        if max_ang > mid_ang:
+            max_node = TreeNode(max_gmrc, self.cgf_manager.copy(),
+                                self, self.g_depth + 1)
+            members.append(max_node)
+
+        is_w_grip = new_gmrc.is_grip_w[grip]
+        all_Gamma_final_idxs = self.cgf_manager.get_angle_idxes(gf, gt, is_w_grip)
+        for idx in all_Gamma_final_idxs:
+            # "bc" means building correspondence here
+            bc_cgf_manager = self.cgf_manager.copy()
+            ang = bc_cgf_manager.get_angle(gf, gt, idx)
+            if ang <= min_ang and ang >= max_ang:
+                continue
+            bc_gmrc = new_gmrc.copy()
+            bc_node = TreeNode(bc_gmrc, bc_cgf_manager, self, self.g_depth + 1)
+            members.append(bc_node)
+
         return members
 
 class CMRRP:
